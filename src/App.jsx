c9fb1bdde18a2fba0301b04ee8b2d28bbc014874
Ankush Ranapure <ar2653@njit.edu>
2024-10-05 22:58:09 -0400

    Added analytics and updated images

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,14 @@ import Navbar from "./components/Navbar";
 import Contact from "./components/Contact";
 import Projects from "./components/Projects";
 import About from "./components/About";
+import posthog from "posthog-js";
 // import Footer from './components/Footer';
 
 function App() {
+  posthog.init("phc_4wRr396iA3G9Tw4lQUd6h0iOF5smmLScLAWBtDMgduq", {
+    api_host: "https://us.i.posthog.com",
+    person_profiles: "identified_only",
+  });
   const [theme, setTheme] = useState("light");
 
   useEffect(() => {
@@ -23,12 +28,12 @@ function App() {
     <div className="min-h-screen flex flex-col">
       <Router>
         <Navbar theme={theme} setTheme={setTheme} />
-          <Routes>
-            <Route path="/" element={<Intro theme={theme} />} /> 
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/projects" element={<Projects />} />
-          </Routes>
+        <Routes>
+          <Route path="/" element={<Intro theme={theme} />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
       </Router>
     </div>
   );
--- src/data/projects.js
+++ src/data/projects.js
@@ -6,7 +6,7 @@ const projects = {
           "An e-commerce platform offering a wide range of kitchenware and home decor, providing customers with high-quality products for cooking and entertaining. With a focus on craftsmanship, Williams Sonoma delivers premium kitchen essentials and stylish decor items, enhancing the culinary experience and transforming living spaces.",
         type: "projects",
         image:
-          "https://th.bing.com/th/id/R.c20961ba13f0eb2f34aabb7974c420dc?rik=BVN%2bVb990BLAiQ&riu=http%3a%2f%2f2.bp.blogspot.com%2f-no4Z2cw4Mpo%2fU9gNKjina9I%2fAAAAAAAAKDk%2f0ISJ3Trsalo%2fs1600%2fWilliamsSonoma_Gift_Card.JPG&ehk=TUhiGEOzIYXUSgm2YhJjoNtcyIhOpVliYRJKAzV0QaY%3d&risl=&pid=ImgRaw&r=0",
+          "https://ankush-portfolio-2.s3.amazonaws.com/williamssonoma.jpeg",
       },
       {
         name: "Westelm",
@@ -14,7 +14,7 @@ const projects = {
           "A modern home furnishings retailer specializing in stylish and sustainable furniture, decor, and accessories for a contemporary lifestyle. Westelm focuses on ethically sourced products, collaborating with artisans to create unique pieces that enhance living spaces while promoting a sustainable and eco-friendly approach.",
         type: "projects",
         image:
-          "https://freight.cargo.site/t/original/i/fd39064b5cce176ef1f4bde0570aab2eb45bed5e1a3c1a9828a3833b9d2b228b/WestElm_LOGO_v1.jpg",
+          "https://ankush-portfolio-2.s3.amazonaws.com/westelm.jpg",
       },
       {
         name: "Pottery Barn Kids",
@@ -29,21 +29,21 @@ const projects = {
           "Schedule.ly is an appointment scheduling software designed to streamline bookings for businesses. It offers features like calendar integrations, automated reminders, and built-in video conferencing, helping businesses save time and reduce no-shows. The platform's seamless payment collection ensures an efficient scheduling process.",
         type: "projects",
         image:
-          "https://storage.googleapis.com/edis-assets.appspot.com/img/iso/cta.png",
+          "https://ankush-portfolio-2.s3.amazonaws.com/sc.png",
       },
       {
         name: "Projects.ly",
         description:
           "Projects.ly is a project management tool that helps teams organize tasks using multiple views like Kanban, Calendar, and Gantt charts. It includes advanced workflow automation, time tracking, and integration with collaboration tools, enabling efficient remote team management and real-time project monitoring for scaling businesses.",
         type: "projects",
-        image: "https://i.ytimg.com/vi/BJXmxI0EOlc/maxresdefault.jpg",
+        image: "https://ankush-portfolio-2.s3.amazonaws.com/ply.jpg",
       },
       {
         name: "Moya the Waterman",
         description:
           "Moya is an order management and delivery tracking application that provides real-time updates on order statuses. It facilitates communication between customer support, distributors, customers, and suppliers, ensuring seamless operations. Moya plays a crucial role in maintaining supply chain transparency and improving customer satisfaction by resolving issues effectively.",
         type: "projects",
-        image: "https://ankush-portfolio.s3.amazonaws.com/moya.jpeg",
+        image: "https://ankush-portfolio-2.s3.amazonaws.com/moya.jpeg",
       },
       {
         name: "Make My Space Trip",
@@ -57,7 +57,7 @@ const projects = {
         description:
           "Wave Dynamics is an innovative web application that simulates and visualizes ocean wave behaviors using real-world data. It features interactive controls that allow users to manipulate wave parameters, making it an ideal educational tool for students, researchers, and coastal planners, promoting engagement and awareness of ocean sciences.",
         type: "sideprojects",
-        image: "https://ankush-portfolio.s3.amazonaws.com/wd.png",
+        image: "https://ankush-portfolio-2.s3.amazonaws.com/wavedynamics.png",
       },
       {
         name: "Engage EDU",
@@ -79,7 +79,7 @@ const projects = {
           "Implemented various types of gRPC calls, including unary, server streaming, client streaming, and bidirectional streaming. The application establishes a secure connection and utilizes efficient communication protocols to handle different streaming scenarios, demonstrating the flexibility and power of gRPC for real-time data exchange in modern applications.",
         type: "sideprojects",
         image:
-        "https://miro.medium.com/max/3856/1*_f2UB-ONG4lN1P1OLfXcZA.png",
+        "https://ankush-portfolio-2.s3.amazonaws.com/grpc.png",
       },
     ],
   };
